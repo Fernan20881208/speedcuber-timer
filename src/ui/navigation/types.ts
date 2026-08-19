8aeafd7161f7c965c3967eb4cb257cf7f7d5d790
Zaid.nvr <utegfer@gmail.com>
2026-08-19 13:30:25 -0600

    Add Training route to RootDrawerParamList type

--- src/ui/navigation/types.ts
+++ src/ui/navigation/types.ts
@@ -17,6 +17,7 @@ import { STIF } from '../../lib/stif';
 export type RootDrawerParamList = {
   Practice: undefined;
   Appearance: undefined;
+  Training: undefined;
   Learn: undefined;
   Play: undefined;
   Backup: undefined;
