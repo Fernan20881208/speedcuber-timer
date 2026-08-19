236a8d8a33aa1ea5960397119e0cddf995ce2cae
Zaid.nvr <utegfer@gmail.com>
2026-08-19 14:52:58 -0600

    Refactor AttemptList component structure and imports

--- src/ui/components/attempts/AttemptList.tsx
+++ src/ui/components/attempts/AttemptList.tsx
@@ -1,47 +1,159 @@
 // Copyright (c) 2023 Joseph Hale <me@jhale.dev>
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
-// License, v. 2.0. If a copy of the MPL was not distributed with this
-// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+// License, v. 2.0.
 
-import { Dimensions, FlatList, View } from 'react-native';
+import {
+  Dimensions,
+  FlatList,
+  View,
+} from 'react-native';
 
-import AttemptCard from './AttemptCard';
-import { STIF } from '../../../lib/stif';
-import ZeroAttemptsPlaceholder from './ZeroAttemptsPlaceholder';
+import AttemptCard
+  from './AttemptCard';
 
-const NUM_COLUMNS = 3;
+import {
+  STIF,
+} from '../../../lib/stif';
+
+import ZeroAttemptsPlaceholder
+  from './ZeroAttemptsPlaceholder';
+
+const NUM_COLUMNS =
+  3;
 
 interface AttemptListProps {
-  attempts: ArrayLike<STIF.Attempt>;
-  onPress?: (attempt: STIF.Attempt) => void;
+  attempts:
+    ArrayLike<
+      STIF.Attempt
+    >;
+
+  onPress?: (
+    attempt:
+      STIF.Attempt,
+  ) => void;
+
+  isFavorite?: (
+    attemptId:
+      string,
+  ) => boolean;
+
+  onToggleFavorite?: (
+    attempt:
+      STIF.Attempt,
+  ) => void;
 }
 
-function AttemptListDelegator(props: AttemptListProps) {
-  if (props.attempts.length === 0) {
-    return <ZeroAttemptsPlaceholder />;
-  } else {
-    return <AttemptList {...props} />;
+function AttemptListDelegator(
+  props:
+    AttemptListProps,
+) {
+  if (
+    props.attempts.length ===
+    0
+  ) {
+    return (
+      <ZeroAttemptsPlaceholder />
+    );
   }
-}
 
-function AttemptList({ attempts, onPress = () => {} }: AttemptListProps) {
-  const renderAttempt = ({ item }: { item: STIF.Attempt }) => (
-    <View style={{ width: Dimensions.get('window').width / NUM_COLUMNS }}>
-      <AttemptCard key={item.id} attempt={item} onPress={onPress} />
-    </View>
-  );
   return (
-    <View style={{ flex: 1 }}>
-      <FlatList
-        data={attempts}
-        renderItem={renderAttempt}
-        keyExtractor={attempt => attempt.id}
-        numColumns={NUM_COLUMNS}
-        initialNumToRender={30}
+    <AttemptList
+      {...props}
+    />
+  );
+}
+
+function AttemptList({
+  attempts,
+
+  onPress =
+    () => {},
+
+  isFavorite =
+    () => false,
+
+  onToggleFavorite =
+    () => {},
+}: AttemptListProps) {
+  const renderAttempt = ({
+    item,
+  }: {
+    item:
+      STIF.Attempt;
+  }) => (
+    <View
+      style={{
+        width:
+          Dimensions
+            .get(
+              'window',
+            )
+            .width /
+          NUM_COLUMNS,
+      }}>
+
+      <AttemptCard
+        key={
+          item.id
+        }
+
+        attempt={
+          item
+        }
+
+        onPress={
+          onPress
+        }
+
+        favorite={
+          isFavorite(
+            item.id,
+          )
+        }
+
+        onToggleFavorite={
+          onToggleFavorite
+        }
       />
+
+    </View>
+  );
+
+  return (
+    <View
+      style={{
+        flex: 1,
+      }}>
+
+      <FlatList
+        data={
+          Array.from(
+            attempts,
+          )
+        }
+
+        renderItem={
+          renderAttempt
+        }
+
+        keyExtractor={
+          attempt =>
+            attempt.id
+        }
+
+        numColumns={
+          NUM_COLUMNS
+        }
+
+        initialNumToRender={
+          30
+        }
+      />
+
     </View>
   );
 }
 
-export default AttemptListDelegator;
+export default
+  AttemptListDelegator;
