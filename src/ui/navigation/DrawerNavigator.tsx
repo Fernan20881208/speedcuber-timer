1148a09562a8950ff83e43b7c1f729d2c5030a4f
Zaid.nvr <utegfer@gmail.com>
2026-08-19 13:50:49 -0600

    Refactor imports and update DrawerNavigator structure

--- src/ui/navigation/DrawerNavigator.tsx
+++ src/ui/navigation/DrawerNavigator.tsx
@@ -4,71 +4,135 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import * as React from 'react';
+import * as React
+  from 'react';
+
+import {
+  createDrawerNavigator,
+} from '@react-navigation/drawer';
+
+import {
+  useTranslation,
+} from 'react-i18next';
+
+import BackupScreen
+  from '../screens/BackupScreen';
+
+import ExamplesNavigator
+  from '../examples/ExamplesNavigator';
+
+import FileSystemStackNavigator
+  from '../components/filesystem/FileSystemStackNavigator';
+
+import PlayScreen
+  from '../screens/PlayScreen';
+
+import PracticeNavigator
+  from './PracticeNavigator';
+
+import AppearanceScreen
+  from '../screens/AppearanceScreen';
 
 import TrainingScreen
   from '../screens/TrainingScreen';
-import BackupScreen from '../screens/BackupScreen';
-import ExamplesNavigator from '../examples/ExamplesNavigator';
-import FileSystemStackNavigator from '../components/filesystem/FileSystemStackNavigator';
-import AppearanceScreen
-  from '../screens/AppearanceScreen';
-import PlayScreen from '../screens/PlayScreen';
-import PracticeNavigator from './PracticeNavigator';
-import { RootDrawerParamList } from './types';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { useTranslation } from 'react-i18next';
 
-const Drawer = createDrawerNavigator<RootDrawerParamList>();
+import {
+  RootDrawerParamList,
+} from './types';
 
-const DrawerNavigator: React.FC = () => {
-  const { t } = useTranslation();
+const Drawer =
+  createDrawerNavigator<
+    RootDrawerParamList
+  >();
+
+const DrawerNavigator:
+  React.FC = () => {
+  const {
+    t,
+  } =
+    useTranslation();
+
   return (
-    <Drawer.Screen
-  name="Training"
-  component={TrainingScreen}
-  options={{
-    drawerLabel:
-      'Entrenamiento',
-  }}
-/>
-    <Drawer.Navigator id="Root" screenOptions={{ headerShown: false }}>
-      <Drawer.Screen name="Practice" component={PracticeNavigator} />
+    <Drawer.Navigator
+      id="Root"
+      screenOptions={{
+        headerShown:
+          false,
+      }}>
       <Drawer.Screen
-        name="Backup"
-        component={BackupScreen}
+        name="Practice"
+        component={
+          PracticeNavigator
+        }
+      />
+
+      <Drawer.Screen
+        name="Training"
+        component={
+          TrainingScreen
+        }
         options={{
-          drawerLabel: t('backup.drawerLabel'),
+          drawerLabel:
+            'Entrenamiento',
         }}
       />
+
       <Drawer.Screen
-  name="Appearance"
-  component={AppearanceScreen}
-  options={{
-    drawerLabel: 'Apariencia',
-  }}
-/>
+        name="Appearance"
+        component={
+          AppearanceScreen
+        }
+        options={{
+          drawerLabel:
+            'Apariencia',
+        }}
+      />
+
+      <Drawer.Screen
+        name="Backup"
+        component={
+          BackupScreen
+        }
+        options={{
+          drawerLabel:
+            t(
+              'backup.drawerLabel',
+            ),
+        }}
+      />
+
       {__DEV__ && (
         <>
           <Drawer.Screen
             name="Examples"
-            component={ExamplesNavigator}
+            component={
+              ExamplesNavigator
+            }
             options={{
-              drawerLabel: 'Dev Examples',
+              drawerLabel:
+                'Dev Examples',
             }}
           />
+
           <Drawer.Screen
             name="Play"
-            component={PlayScreen}
+            component={
+              PlayScreen
+            }
             options={{
-              drawerLabel: 'Dev Playground',
+              drawerLabel:
+                'Dev Playground',
             }}
           />
+
           <Drawer.Screen
             name="FileSystemStack"
-            component={FileSystemStackNavigator}
+            component={
+              FileSystemStackNavigator
+            }
             options={{
-              drawerLabel: 'File System',
+              drawerLabel:
+                'File System',
             }}
           />
         </>
