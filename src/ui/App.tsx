45b6cd14b23dbeb90bf66aded683324a15191191
Zaid.nvr <utegfer@gmail.com>
2026-08-19 12:54:47 -0600

    Refactor App component and update imports

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,43 +1,82 @@
-// Copyright (c) 2022 Joseph Hale <me@jhale.dev>
-//
-// This Source Code Form is subject to the terms of the Mozilla Public
-// License, v. 2.0. If a copy of the MPL was not distributed with this
-// file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
 import 'react-native-get-random-values';
 
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import {
+  GestureHandlerRootView,
+} from 'react-native-gesture-handler';
+
 import MainNavigator from './navigation/MainNavigator';
-import { Provider as PaperProvider } from 'react-native-paper';
-import { getCurrentTheme } from './themes';
+
+import {
+  Provider as PaperProvider,
+} from 'react-native-paper';
+
 import i18n from '../localization';
-import { RealmProvider, useRealm } from '../persistence/realmdb';
+
+import {
+  RealmProvider,
+  useRealm,
+} from '../persistence/realmdb';
+
+import {
+  AppearanceProvider,
+  useAppearance,
+} from '../features/appearance/AppearanceContext';
 
 const DevComponents = () => {
   const FlipperAsyncStorage =
-    require('rn-flipper-async-storage-advanced').default;
-  const RealmPlugin = require('realm-flipper-plugin-device').default;
+    require(
+      'rn-flipper-async-storage-advanced',
+    ).default;
+
+  const RealmPlugin =
+    require(
+      'realm-flipper-plugin-device',
+    ).default;
+
   const realm = useRealm();
+
   return (
     <>
       <FlipperAsyncStorage />
-      <RealmPlugin realms={[realm]} />
+
+      <RealmPlugin
+        realms={[realm]}
+      />
     </>
+  );
+};
+
+const ThemedApp = () => {
+  const {
+    theme,
+  } = useAppearance();
+
+  return (
+    <PaperProvider
+      theme={theme}>
+      <RealmProvider>
+        {__DEV__ &&
+          <DevComponents />}
+
+        <MainNavigator />
+      </RealmProvider>
+    </PaperProvider>
   );
 };
 
 const App = () => {
   if (i18n.isInitialized) {
-    console.log('i18n initialized');
+    console.log(
+      'i18n initialized',
+    );
   }
+
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <PaperProvider theme={getCurrentTheme()}>
-        <RealmProvider>
-          {__DEV__ && <DevComponents />}
-          <MainNavigator />
-        </RealmProvider>
-      </PaperProvider>
+    <GestureHandlerRootView
+      style={{flex: 1}}>
+      <AppearanceProvider>
+        <ThemedApp />
+      </AppearanceProvider>
     </GestureHandlerRootView>
   );
 };
