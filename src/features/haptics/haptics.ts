61b79d6873f222ef6816599eb7aa690a7471fe34
Zaid.nvr <utegfer@gmail.com>
2026-08-19 13:09:09 -0600

    Implement haptic feedback functions for Android
    Added functions for haptic feedback on Android devices.

--- src/features/haptics/haptics.ts
+++ src/features/haptics/haptics.ts
@@ -1,1 +1,49 @@
+import {
+  Platform,
+  Vibration,
+} from 'react-native';
 
+export function readyHaptic() {
+  if (
+    Platform.OS !== 'android'
+  ) {
+    return;
+  }
+
+  Vibration.vibrate(25);
+}
+
+export function startHaptic() {
+  if (
+    Platform.OS !== 'android'
+  ) {
+    return;
+  }
+
+  Vibration.vibrate(35);
+}
+
+export function stopHaptic() {
+  if (
+    Platform.OS !== 'android'
+  ) {
+    return;
+  }
+
+  Vibration.vibrate(45);
+}
+
+export function pbHaptic() {
+  if (
+    Platform.OS !== 'android'
+  ) {
+    return;
+  }
+
+  Vibration.vibrate([
+    0,
+    40,
+    60,
+    80,
+  ]);
+}
