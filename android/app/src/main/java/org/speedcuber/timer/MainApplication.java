6b9a269e4ec7f3396c9f0f7803e70656fe945e35
Zaid.nvr <utegfer@gmail.com>
2026-08-19 15:07:02 -0600

    Refactor MainApplication for new architecture support

--- android/app/src/main/java/org/speedcuber/timer/MainApplication.java
+++ android/app/src/main/java/org/speedcuber/timer/MainApplication.java
@@ -1,67 +1,100 @@
 package org.speedcuber.timer;
 
 import android.app.Application;
-import android.content.Context;
+
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
+
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactNativeHost;
+
 import com.facebook.soloader.SoLoader;
-import java.lang.reflect.InvocationTargetException;
+
 import java.util.List;
 
-public class MainApplication extends Application implements ReactApplication {
+import org.speedcuber.timer.liquidglass.ZaidLiquidGlassPackage;
 
-  private final ReactNativeHost mReactNativeHost =
-      new DefaultReactNativeHost(this) {
-        @Override
-        public boolean getUseDeveloperSupport() {
-          return BuildConfig.DEBUG;
-        }
+public class MainApplication
+        extends Application
+        implements ReactApplication {
 
-        @Override
-        protected List<ReactPackage> getPackages() {
-          @SuppressWarnings("UnnecessaryLocalVariable")
-          List<ReactPackage> packages = new PackageList(this).getPackages();
-          // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
-          packages.add(new LocationStatusPackage());
-          return packages;
-        }
+    private final ReactNativeHost mReactNativeHost =
+            new DefaultReactNativeHost(this) {
 
-        @Override
-        protected String getJSMainModuleName() {
-          return "index";
-        }
+                @Override
+                public boolean getUseDeveloperSupport() {
+                    return BuildConfig.DEBUG;
+                }
 
-        @Override
-        protected boolean isNewArchEnabled() {
-          return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
-        }
+                @Override
+                protected List<ReactPackage> getPackages() {
+                    List<ReactPackage> packages =
+                            new PackageList(this).getPackages();
 
-        @Override
-        protected Boolean isHermesEnabled() {
-          return BuildConfig.IS_HERMES_ENABLED;
-        }
-      };
+                    /*
+                     * Paquetes que no se autolinkean
+                     * se registran manualmente aquí.
+                     */
 
-  @Override
-  public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
-  }
+                    packages.add(
+                            new LocationStatusPackage()
+                    );
 
-  @Override
-  public void onCreate() {
-    super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
-    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      // If you opted-in for the New Architecture, we load the native entry point for this app.
-      DefaultNewArchitectureEntryPoint.load();
+                    /*
+                     * Zaid Liquid Glass
+                     * React Native -> Android native bridge
+                     */
+
+                    packages.add(
+                            new ZaidLiquidGlassPackage()
+                    );
+
+                    return packages;
+                }
+
+                @Override
+                protected String getJSMainModuleName() {
+                    return "index";
+                }
+
+                @Override
+                protected boolean isNewArchEnabled() {
+                    return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
+                }
+
+                @Override
+                protected Boolean isHermesEnabled() {
+                    return BuildConfig.IS_HERMES_ENABLED;
+                }
+            };
+
+    @Override
+    public ReactNativeHost getReactNativeHost() {
+        return mReactNativeHost;
     }
-    ReactNativeFlipper.initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
-  }
-}
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        SoLoader.init(
+                this,
+                false
+        );
+
+        if (
+                BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+        ) {
+            DefaultNewArchitectureEntryPoint.load();
+        }
+
+        ReactNativeFlipper.initializeFlipper(
+                this,
+                getReactNativeHost()
+                        .getReactInstanceManager()
+        );
+    }
+        }
