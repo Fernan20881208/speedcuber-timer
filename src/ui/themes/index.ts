806ab424dec00959ce91ec0f89ebc7d6e5a53913
Zaid.nvr <utegfer@gmail.com>
2026-08-19 12:53:29 -0600

    Refactor theme management and update getTheme function

--- src/ui/themes/index.ts
+++ src/ui/themes/index.ts
@@ -1,50 +1,85 @@
-// Copyright (c) 2022 Joseph Hale <me@jhale.dev>
-//
-// This Source Code Form is subject to the terms of the Mozilla Public
-// License, v. 2.0. If a copy of the MPL was not distributed with this
-// file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
 import {
-  MD3DarkTheme,
-  MD3LightTheme,
   adaptNavigationTheme,
 } from 'react-native-paper';
+
 import {
   DarkTheme as NavigationDarkTheme,
   DefaultTheme as NavigationDefaultTheme,
 } from '@react-navigation/native';
 
-import { Appearance } from 'react-native';
-import { colorThemes } from './colors';
-import { fonts } from './fonts';
+import {ColorSchemeName} from 'react-native';
 import merge from 'deepmerge';
 
-const { LightTheme, DarkTheme } = adaptNavigationTheme({
+import {
+  zaidAmoledTheme,
+  zaidDarkTheme,
+  zaidLightTheme,
+} from './zaidThemes';
+
+import {fonts} from './fonts';
+
+import type {
+  AppearanceMode,
+} from '../../features/appearance/appearanceStorage';
+
+const {
+  LightTheme,
+  DarkTheme,
+} = adaptNavigationTheme({
   reactNavigationLight: NavigationDefaultTheme,
   reactNavigationDark: NavigationDarkTheme,
 });
 
+const combinedLightTheme =
+  merge(LightTheme, zaidLightTheme);
 
+const combinedDarkTheme =
+  merge(DarkTheme, zaidDarkTheme);
 
+const combinedAmoledTheme =
+  merge(DarkTheme, zaidAmoledTheme);
 
-function getCurrentTheme() {
-  const RNPLightTheme = colorThemes.green.light
-  const RNPDarkTheme = colorThemes.green.dark
-  const CombinedLightTheme = merge(LightTheme, RNPLightTheme);
-  const CombinedDarkTheme = merge(DarkTheme, RNPDarkTheme);
+export function getTheme(
+  mode: AppearanceMode,
+  systemScheme: ColorSchemeName,
+) {
+  let resolvedMode: AppearanceMode = mode;
 
-  const darkTheme = Appearance.getColorScheme() == 'dark';
-  const baseTheme = darkTheme ? CombinedDarkTheme : CombinedLightTheme;
-  const theme = {
+  if (mode === 'system') {
+    resolvedMode =
+      systemScheme === 'dark'
+        ? 'dark'
+        : 'light';
+  }
+
+  let baseTheme;
+
+  switch (resolvedMode) {
+    case 'light':
+      baseTheme = combinedLightTheme;
+      break;
+
+    case 'amoled':
+      baseTheme = combinedAmoledTheme;
+      break;
+
+    case 'liquidGlass':
+      // Temporal.
+      // Liquid Glass real se agregará después.
+      baseTheme = combinedDarkTheme;
+      break;
+
+    case 'dark':
+    default:
+      baseTheme = combinedDarkTheme;
+      break;
+  }
+
+  return {
     ...baseTheme,
-    roundness: 2,
-    colors: {
-      ...baseTheme.colors,
-      secondary: darkTheme ? '#00ff00' : '#1B5E20',
-    },
-    fonts: fonts,
-  };
-  return theme;
-}
 
-export { getCurrentTheme };
+    roundness: 2,
+
+    fonts,
+  };
+}
