6df3ec71db9cf29ea768e53178115eb54ec854b3
Zaid.nvr <utegfer@gmail.com>
2026-08-21 22:24:31 +0000

    Stabilize main solve timer interval

--- src/ui/components/SolveTimer.tsx
+++ src/ui/components/SolveTimer.tsx
@@ -4,33 +4,48 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import { Pressable, StyleSheet } from 'react-native';
-import { useEffect, useState } from 'react';
+import {Pressable, StyleSheet} from 'react-native';
+import {useEffect, useRef, useState} from 'react';
 import * as React from 'react';
 
 import Time from './Time';
-import { Timer } from '../../lib/timers';
+import {Timer} from '../../lib/timers';
 
 interface SolveTimerProps {
   onStopTimer: (duration: Date) => void;
 }
 
-const SolveTimer: React.FC<SolveTimerProps> = ({ onStopTimer }) => {
+const SolveTimer: React.FC<SolveTimerProps> = ({onStopTimer}) => {
   const [timer] = useState(new Timer());
   const [elapsed, setElapsed] = useState(new Date(0));
+  const stoppedRef = useRef(false);
+
   useEffect(() => {
-    if (!timer.isRunning()) {
+    if (!timer.isStarted()) {
       timer.start();
     }
+
     const interval = setInterval(() => {
       setElapsed(new Date(timer.elapsedMilliseconds()));
     }, 20);
+
     return () => clearInterval(interval);
-  });
+  }, [timer]);
 
   function handleOnPressIn() {
-    timer.reset();
-    onStopTimer(elapsed);
+    if (stoppedRef.current) {
+      return;
+    }
+
+    stoppedRef.current = true;
+
+    if (timer.isRunning()) {
+      timer.stop();
+    }
+
+    const finalElapsed = new Date(timer.elapsedMilliseconds());
+    setElapsed(finalElapsed);
+    onStopTimer(finalElapsed);
   }
 
   return (
