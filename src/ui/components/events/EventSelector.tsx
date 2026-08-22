fd9482594e23c10fd67bc2317c6593462f90971f
Zaid.nvr <utegfer@gmail.com>
2026-08-22 00:28:52 +0000

    Fix event selector controls overlapping rows

--- src/ui/components/events/EventSelector.tsx
+++ src/ui/components/events/EventSelector.tsx
@@ -1,19 +1,18 @@
 // Copyright (c) 2022 Joseph Hale <me@jhale.dev>
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
-// License, v. 2.0. If a copy of the MPL was not distributed with this
-// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+// License, v. 2.0.
 
 import * as Events from '../../../lib/stif/builtins/CompetitiveEvents';
 
-import { Divider, List } from 'react-native-paper';
-import { Fragment, useCallback, useState } from 'react';
-import { ScrollView, View } from 'react-native';
+import {Divider, List} from 'react-native-paper';
+import {Fragment, useCallback, useState} from 'react';
+import {ScrollView, StyleSheet, View} from 'react-native';
 
 import Icons from '../../icons/iconHelper';
-import { STIF } from '../../../lib/stif';
+import {STIF} from '../../../lib/stif';
 import Ticker from '../ticker/Ticker';
-import { useTranslation } from 'react-i18next';
+import {useTranslation} from 'react-i18next';
 
 interface EventSelectorProps {
   onSelect: (event: STIF.CompetitiveEvent) => void;
@@ -24,13 +23,17 @@ interface EventItemProps {
   onSelect: (event: STIF.CompetitiveEvent) => void;
 }
 
-function EventItem({ event, onSelect }: EventItemProps) {
-  const { t } = useTranslation();
+function EventItem({event, onSelect}: EventItemProps) {
+  const {t} = useTranslation();
   const [multiCount, setMultiCount] = useState(2);
-  const isMultiEvent = () => ['333mbf', "333m", "222m"].includes(event.id)
+  const isMultiEvent = () => ['333mbf', '333m', '222m'].includes(event.id);
+
   const pressHandler = useCallback(() => {
     if (isMultiEvent()) {
-      onSelect({ ...event, puzzles: new Array(multiCount).fill(event.puzzles[0]) });
+      onSelect({
+        ...event,
+        puzzles: new Array(multiCount).fill(event.puzzles[0]),
+      });
     } else {
       onSelect(event);
     }
@@ -40,16 +43,23 @@ function EventItem({ event, onSelect }: EventItemProps) {
     <List.Item
       title={t(`events.${event.id}`)}
       onPress={pressHandler}
-      left={props => <List.Icon {...props} icon={Icons.STIF(`event-${event.id}`)} />}
-      right={props =>
+      style={styles.item}
+      left={props => (
+        <List.Icon
+          {...props}
+          icon={Icons.STIF(`event-${event.id}`)}
+        />
+      )}
+      right={() =>
         isMultiEvent() ? (
-          <View style={{margin: -14}}>
+          <View style={styles.tickerContainer}>
             <Ticker
               initialValue={2}
               min={2}
               step={1}
               onChange={value => setMultiCount(value)}
               orientation="horizontal"
+              iconStyle={styles.tickerIcon}
             />
           </View>
         ) : null
@@ -58,50 +68,75 @@ function EventItem({ event, onSelect }: EventItemProps) {
   );
 }
 
-export default function EventSelector({ onSelect }: EventSelectorProps) {
-  const { t } = useTranslation();
+export default function EventSelector({onSelect}: EventSelectorProps) {
+  const {t} = useTranslation();
   const unsupportedEvents = ['unknown', '333bf-team', '23relay'];
   const events = Object.values(Events).filter(
     e => !unsupportedEvents.includes(e.id),
   );
+
   return (
-    <ScrollView>
-      <List.Section>
+    <ScrollView
+      showsVerticalScrollIndicator={false}
+      contentContainerStyle={styles.content}>
+      <List.Section style={styles.section}>
         <List.Subheader>{t('event.type.official')}</List.Subheader>
         {events
           .filter(e => e.type === 'official')
           .map((e, idx) => (
-            // Inspired by: https://www.codemzy.com/blog/joining-arrays-react-components
             <Fragment key={e.id}>
               {!!idx && <Divider horizontalInset />}
-              <EventItem key={e.id} event={e} onSelect={onSelect} />
+              <EventItem event={e} onSelect={onSelect} />
             </Fragment>
           ))}
       </List.Section>
-      <List.Section>
+
+      <List.Section style={styles.section}>
         <List.Subheader>{t('event.type.unofficial')}</List.Subheader>
         {events
           .filter(e => e.type === 'unofficial')
           .map((e, idx) => (
-            // Inspired by: https://www.codemzy.com/blog/joining-arrays-react-components
             <Fragment key={e.id}>
               {!!idx && <Divider horizontalInset />}
-              <EventItem key={e.id} event={e} onSelect={onSelect} />
+              <EventItem event={e} onSelect={onSelect} />
             </Fragment>
           ))}
       </List.Section>
-      <List.Section>
+
+      <List.Section style={styles.section}>
         <List.Subheader>{t('event.type.retired')}</List.Subheader>
         {events
           .filter(e => e.type === 'retired')
           .map((e, idx) => (
-            // Inspired by: https://www.codemzy.com/blog/joining-arrays-react-components
             <Fragment key={e.id}>
               {!!idx && <Divider horizontalInset />}
-              <EventItem key={e.id} event={e} onSelect={onSelect} />
+              <EventItem event={e} onSelect={onSelect} />
             </Fragment>
           ))}
       </List.Section>
     </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  content: {
+    paddingBottom: 12,
+  },
+
+  section: {
+    marginVertical: 0,
+  },
+
+  item: {
+    minHeight: 58,
+  },
+
+  tickerContainer: {
+    alignSelf: 'center',
+    marginRight: 4,
+  },
+
+  tickerIcon: {
+    margin: 0,
+  },
+});
